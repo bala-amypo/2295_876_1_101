ceb05fddddc1697fe1962d2be1268ef695fc747c
bala-amypo <bala@amypo.in>
2025-12-24 04:11:13 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,21 +1,18 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI openAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        // new Server().url("https://9633.pro604cr.amypo.ai/")
-                        new Server().url("https://9184.32procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Smart Inventory Restock Predictor API")
+                        .version("1.0"));
+    }
 }
