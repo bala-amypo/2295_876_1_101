eafe74d22c71cc9f6ec47a34d9228fdcbea9d053
bala-amypo <bala@amypo.in>
2025-12-29 04:09:36 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -1,6 +1,10 @@
 package com.example.demo.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
 
@@ -10,7 +14,9 @@ import java.util.Set;
 @AllArgsConstructor
 @Builder
 public class AuthResponse {
+
     private Long id;
     private String email;
     private Set<String> roles;
+    private String token;
 }
